a2e27861afbe52ed91d1c79dd9130e968025773b
kyleMeyers <kyleMeyers22@outlook.com>
2016-09-25 18:28:02 -0400

    updated the backend stuff to test locally on physical device as well

--- app/src/main/java/kylemeyers22/baseballsimandroid/MainActivity.java
+++ app/src/main/java/kylemeyers22/baseballsimandroid/MainActivity.java
@@ -34,7 +34,9 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+
+                    //this is my computers IP Address use yours to test locally or use 10.0.2.2
+                    .setRootUrl("http://192.168.154.34:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
                         public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {

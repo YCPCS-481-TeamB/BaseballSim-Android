eb2121db9aba647b9bee60a4e45f24e501fdc299
bcoov <bcoover88@gmail.com>
2016-10-06 20:05:34 -0400

    Working authentication IN PROGRESS
    URLConnection object is getting params correctly, but the params are not currently being passed successfully to the POST request.

--- heroku/src/main/java/kylemeyers22/heroku/LoginActivity.java
+++ heroku/src/main/java/kylemeyers22/heroku/LoginActivity.java
@@ -3,12 +3,21 @@ package kylemeyers22.heroku;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by Ben Coover on 10/4/2016.
@@ -37,25 +46,70 @@ public class LoginActivity extends AppCompatActivity {
         private ProgressDialog Dialog = new ProgressDialog(LoginActivity.this);
         private TextView usernameInput = (TextView) findViewById(R.id.UsernameInput);
         private TextView passwordInput = (TextView) findViewById(R.id.PasswordInput);
-        private String uname, pword;
+        private String uname, pword, authToken;
+
+        SharedPreferences sharedPref = LoginActivity.this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
 
         protected void onPreExecute() {
-            uname = usernameInput.getText().toString();
-            pword = usernameInput.getText().toString();
+//            uname = usernameInput.getText().toString();
+//            pword = passwordInput.getText().toString();
+
+            // FOR DEBUGGING
+            uname = "bcoover";
+            pword = "devpass";
 
             Dialog.setMessage("Logging in...");
             Dialog.show();
         }
 
         protected Void doInBackground(String... urls) {
+            URL getToken;
+            HttpURLConnection conn;
+
+            try {
+                System.out.println("---- IN LoginActivity.doInBackground");
+                getToken = new URL(urls[0]);
+                String authParams = "username=" + uname + "&password=" + pword;
+                byte[] authData = authParams.getBytes("UTF-8");
+                conn = (HttpURLConnection) getToken.openConnection();
+
+                System.out.println("---- ATTEMPTING POST");
+                System.out.println("---- PARAMS ARE: " + authParams);
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-type", "x-www-form-urlencoded");
+                conn.setFixedLengthStreamingMode(authData.length);
+
+                OutputStream postData = conn.getOutputStream();
+
+                postData.write(authData);
+                postData.flush();
+                postData.close();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sBuilder = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    sBuilder.append(line);
+                }
+                authToken = sBuilder.toString();
+            } catch (IOException exc) {
+                exc.printStackTrace();
+            }
+
             return null;
         }
 
         protected void onPostExecute(Void unused) {
             Dialog.dismiss();
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+//            finish();
+
+            System.out.println("---- CHECKING TOKEN");
+            System.out.println(authToken);
         }
     }
 }

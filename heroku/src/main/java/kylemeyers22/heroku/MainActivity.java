724ee51642d88316c1859db6b89886e5f8efab3d
bcoov <bcoover88@gmail.com>
2016-10-23 20:57:02 -0400

    Tidy up MainActivity

--- heroku/src/main/java/kylemeyers22/heroku/MainActivity.java
+++ heroku/src/main/java/kylemeyers22/heroku/MainActivity.java
@@ -9,15 +9,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_activity);
         playerListView = (ListView) findViewById(R.id.playersList);
 
-        //used with postgresexample
-        //final TextView playerFirstLabel = (TextView) findViewById(R.id.playerFirstNameLabel);
         final Button getPlayerButton = (Button) findViewById(R.id.getPlayerButton);
 
         getPlayerButton.setOnClickListener(new View.OnClickListener() {
@@ -58,11 +53,6 @@ public class MainActivity extends AppCompatActivity {
         private ProgressDialog Dialog = new ProgressDialog(MainActivity.this);
         private ArrayAdapter<String> listAdapter;
 
-        String data = "";
-//        TextView uiUpdate = (TextView) findViewById(R.id.playerFirstName);
-//        TextView jsonParsed = (TextView) findViewById(R.id.playerLastName);
-//        TextView serverText = (TextView) findViewById(R.id.serverText);
-
         // Obtain API Authentication Token from LoginActivity's shared preferences
         SharedPreferences sPref = getSharedPreferences("LoginActivity", MODE_PRIVATE);
         final String apiToken = sPref.getString("apiToken", null);
@@ -71,13 +61,6 @@ public class MainActivity extends AppCompatActivity {
             //start progress dialog message
             Dialog.setMessage("Please Wait...");
             Dialog.show();
-
-//            try {
-//                //set request parameter
-//                data += "&" + URLEncoder.encode("data", "UTF-8") + "=" + serverText.getText();
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
         }
 
         protected Void doInBackground(String... urls) {
@@ -95,18 +78,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(Void unused) {
             //close progress dialog
             Dialog.dismiss();
-            //Intent intent = new Intent(MainActivity.this, FieldActivity.class);
-           // startActivity(intent);
-            //finish();
-//
-//            if (Error != null) {
-//                uiUpdate.setText("Output : " + Error);
-//            } else {
-//                uiUpdate.setText(Content);
-
             //Receive JSON response
-            String OutputData;
-
             System.out.println("#---- IN onPostExecute ----#");
             System.out.println(Content);
 
@@ -114,21 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 JSONObject jObj = new JSONObject(Content);
-                System.out.println("##########");
-//                    System.out.println(jObj.getJSONArray("users").getJSONObject(0).getString("firstname"));
-
-//                    OutputData = "Output captured: " + jObj.getJSONArray("users").getJSONObject(0).getString("firstname");
-
-                OutputData = jObj.toString();
-                //show output on screen
-                //jsonParsed.setText(OutputData);
-                //System.out.println(OutputData);
                 JSONArray playersArray = jObj.getJSONArray("players");
                 for (int i = 0; i < playersArray.length(); ++i) {
                     JSONObject item = playersArray.getJSONObject(i);
-//                    System.out.println("PLAYER_FIRST: " + item.getString("firstname"));
-//                    System.out.println("PLAYER_LAST: " + item.getString("lastname"));
-//                    System.out.println("----------");
                     playerList.add(item.getString("firstname") + " " + item.getString("lastname"));
                 }
             } catch (JSONException e) {
@@ -138,8 +98,6 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(playerList.size());
             listAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.listrow, playerList);
             playerListView.setAdapter(listAdapter);
-
-//            }
         }
     }
 }

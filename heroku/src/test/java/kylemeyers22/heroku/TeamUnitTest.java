eef92da26a6cbdb56d4446c215a7d57c1590eb64
kyleMeyers <kyleMeyers22@outlook.com>
2016-10-23 22:24:17 -0400

    the continuation of tests using robolectric now

--- heroku/src/test/java/kylemeyers22/heroku/TeamUnitTest.java
+++ heroku/src/test/java/kylemeyers22/heroku/TeamUnitTest.java
@@ -1,8 +1,12 @@
 
 package kylemeyers22.heroku;
+import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import android.content.SharedPreferences;
 
@@ -15,6 +19,9 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 
 import java.io.IOException;
@@ -28,22 +35,26 @@ import kylemeyers22.heroku.TeamActivity;
 /**
  * Created by shdw2 on 10/23/2016.
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE, sdk = 23)
+//@Config(constants = BuildConfig.class)
 
-public class TeamUnitTest extends TeamActivity{
+public class TeamUnitTest{
 
     @Test
     public void testTeamName()
     {
         try {
             String jsonMimeType = "application/json";
-            HttpUriRequest request = new HttpGet("https://baseballsim.herokuapp.com/api/teams");
+            HttpUriRequest request = new HttpGet("https://baseballsim.herokuapp.com/api/users/token");
 
             // When
             HttpResponse response = HttpClientBuilder.create().build().execute(request);
 
             // Then
-            String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
-            assertEquals(jsonMimeType, mimeType);
+            assertThat(response.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_NOT_FOUND));
+           // String mimeType = ContentType.getOrDefault(response.getEntity()).getMimeType();
+            //assertEquals(jsonMimeType, mimeType);
         }
         catch(IOException e)
         {

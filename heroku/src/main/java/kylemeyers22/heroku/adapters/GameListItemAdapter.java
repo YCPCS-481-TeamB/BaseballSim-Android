3ab5df710ea797fe36423d1e31e99cdc2f83b047
bcoov <bcoover88@gmail.com>
2016-11-23 16:38:16 -0500

    Correct some IDE warnings with getView

--- heroku/src/main/java/kylemeyers22/heroku/adapters/GameListItemAdapter.java
+++ heroku/src/main/java/kylemeyers22/heroku/adapters/GameListItemAdapter.java
@@ -1,12 +1,12 @@
 package kylemeyers22.heroku.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -22,12 +22,13 @@ public class GameListItemAdapter extends ArrayAdapter<Game> {
         this.gameList = gameItems;
     }
 
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    @NonNull
+    public View getView(final int position, View convertView, @NonNull ViewGroup parent) {
         View convert = convertView;
 
         if (convert == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convert = inflater.inflate(R.layout.gamerow, null);
+            convert = inflater.inflate(R.layout.gamerow, parent, false);
         }
 
         final Game current = gameList.get(position);

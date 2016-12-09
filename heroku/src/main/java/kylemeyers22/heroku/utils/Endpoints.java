29e89c4954636628c75d90833068aa9fc7ede5c5
Ben Coover <bcoover@ycp.edu>
2016-12-09 14:44:54 -0500

    Tidy up Endpoints to be more clear

--- heroku/src/main/java/kylemeyers22/heroku/utils/Endpoints.java
+++ heroku/src/main/java/kylemeyers22/heroku/utils/Endpoints.java
@@ -9,30 +9,50 @@ public class Endpoints {
     public static final String authAPI = usersAPI + "token/";
     public static final String playersAPI = rootAPI + "players/";
     public static final String teamsAPI = rootAPI + "teams/";
-    public static final String gamesAPI = rootAPI + "games/";
     public static final String fieldsAPI = rootAPI + "fields/";
     public static final String leaguesAPI = rootAPI + "leagues/";
     public static final String schedulesAPI = rootAPI + "schedules/";
 
-    private static final String approvalAPI = rootAPI + "approvals/";
-    public static final String userPendingApprovalsAPI = approvalAPI + "user/pending";
-
+    /**
+     * Game API Endpoints
+     */
+    public static final String gamesAPI = rootAPI + "games/";
+    // Get game approval status
+    public static String gameIsApprovedAPI(int gameID) {
+        return gamesAPI + Integer.toString(gameID) + "/approvals/state";
+    }
+    // Start game
     public static String gameStartAPI(int gameID) {
         return gamesAPI + Integer.toString(gameID) + "/start/";
     }
-
+    // Get all events for given game
+    public static String gameEventsAllAPI(int gameID) {
+        return gamesAPI + Integer.toString(gameID) + "/events/";
+    }
+    // Trigger next event for game
     public static String gameNextAPI(int gameID) {
         return gamesAPI + Integer.toString(gameID) + "/events/next/";
     }
-
-    public static String userTeamsAPI(int userID) {
-        return usersAPI + Integer.toString(userID) + "/teams";
-    }
-
+    // Get all games for a user
     public static String userGamesAPI(int userID) {
         return usersAPI + Integer.toString(userID) + "/games";
     }
 
+    /**
+     * Team API Endpoints
+     */
+    // Get all teams for a user
+    public static String userTeamsAPI(int userID) {
+        return usersAPI + Integer.toString(userID) + "/teams";
+    }
+
+    /**
+     * Approval API Endpoints
+     */
+    private static final String approvalAPI = rootAPI + "approvals/";
+    // Get all pending approvals for currently logged in user
+    public static final String userPendingApprovalsAPI = approvalAPI + "user/pending";
+    // Set status of an approval
     public static String approvalSetStatusAPI(int approvalID) {
         return approvalAPI + Integer.toString(approvalID) + "/status";
     }
